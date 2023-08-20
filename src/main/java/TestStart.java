fcfdc5faecc7f5d4fe563eacd18004793633b4a6
VitaliyJV <114652130+VitaliyJV@users.noreply.github.com>
2023-08-20 12:20:18 +0300

    2
    2

--- src/main/java/TestStart.java
+++ src/main/java/TestStart.java
@@ -7,46 +7,39 @@ import Service.TicketCrudService;
 import org.flywaydb.core.Flyway;
 import org.hibernate.cfg.Configuration;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
 public class TestStart {
     public static void main(String[] args) {
-        Configuration conf1 = new Configuration();
-        Flyway flyway = Flyway.configure().dataSource(conf1.getProperty("url"), conf1.getProperty("username"), conf1.getProperty("password")).load();
+
+        Properties prop = new Properties();
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream("src/main/resources/hibernate.properties");
+        } catch (FileNotFoundException e) {
+            System.out.println("Config file not found!");
+            throw new RuntimeException(e);
+        }
+        try {
+            prop.load(fileInputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Flyway flyway = Flyway.configure().dataSource(prop.getProperty("hibernate.connection.url") , prop.getProperty("hibernate.connection.user"), prop.getProperty("hibernate.connection.password")).load();
         flyway.migrate();
 
+        ClientCrudService clientCrudService = new ClientCrudService();
+        PlanetCrudService planetCrudService = new PlanetCrudService();
+        TicketCrudService ticketCrudService = new TicketCrudService();
 
+        System.out.println(ticketCrudService.get(1L));
 
+        HibernateUtil.getInstance().close();
 
-
-
-
-
-
-//        ClientCrudService clientCrudService = new ClientCrudService();
-//        PlanetCrudService planetCrudService = new PlanetCrudService();
-//        TicketCrudService ticketCrudService = new TicketCrudService();
-//
-//        System.out.println(clientCrudService.get(2L));
-//        Client client = clientCrudService.get(4L).orElseThrow();
-//        client.setName("Update Name");
-//        clientCrudService.update(client);
-//        System.out.println(clientCrudService.get(4L));
-//        Client newClient = Client.builder().name("New client").build();
-//        clientCrudService.create(newClient);
-//        System.out.println(clientCrudService.get(11L));
-//        clientCrudService.delete(6L);
-//        System.out.println("Client id=3 has next tickets: " + clientCrudService.get(3L).orElseThrow().getTickets());
-//
-//        System.out.println(planetCrudService.get("CAL"));
-//        System.out.println("Planet id=CAL has next tickets to itself: " + planetCrudService.get("CAL").orElseThrow().getToPlanets());
-//        System.out.println("Planet id=CAL has next tickets from itself: " + planetCrudService.get("CAL").orElseThrow().getFromPlanets());
-//
-//        System.out.println(ticketCrudService.get(1L));
-//        Ticket ticket = ticketCrudService.get(1L).orElseThrow();
-//        ticket.setClient(clientCrudService.get(7L).orElseThrow());
-//        ticketCrudService.update(ticket);
-//        System.out.println(ticketCrudService.get(1L));
-//
-//        HibernateUtil.getInstance().close();
 
     }
 }
